c6fab761d0f2592fe5aaacd17ac43d57915ad148
Jeno Labodi <79979904+J-Labodi@users.noreply.github.com>
2023-05-09 11:59:58 +0100

    Update locations.php

--- resources/views/locations.php
+++ resources/views/locations.php
@@ -13,7 +13,7 @@ include('locations_logic.php');
   <meta name="viewport" content="width=device-width", initial-scale="1.0">
   <title>Locations</title>
   <script src="https://kit.fontawesome.com/6f214ab12c.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="style-locations.css">
+  <link rel="stylesheet" href="../css/style-locations.css">
 </head>
 <body>
     <div class="search-bar">
